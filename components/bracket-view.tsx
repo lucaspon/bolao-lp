c74d558f12a6a066d6e980b2e856d03acf44eabf
lucaspon <lucaspon.olv@gmail.com>
2026-06-27 13:42:53 -0300

    add vertical gap to brackets

--- components/bracket-view.tsx
+++ components/bracket-view.tsx
@@ -37,7 +37,7 @@ function RoundColumn({
   return (
     <div className="flex flex-col justify-around" style={{ height: HALF_HEIGHT }}>
       {nos.map((no) => (
-        <div key={no} className="flex items-center py-[1px]">
+        <div key={no} className="flex items-center py-0.5">
           <Pill pill={byNo.get(no)} />
         </div>
       ))}
